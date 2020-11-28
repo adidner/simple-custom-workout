e49aaed6b23154c8d80169b388e4c3340a9f63ca
Aaron Didner <didnera@oregonstate.edu>
2020-11-28 13:29:09 -0500

    started add exercise modal

--- src/components/AddExerciseModal.tsx
+++ src/components/AddExerciseModal.tsx
@@ -1,8 +1,52 @@
-import React from 'react';
-import {Text} from 'react-native';
+import React, { useState } from 'react';
+import {Text, Modal, View, TouchableOpacity} from 'react-native';
+import { exerciseElement } from '../../constants/interfaces';
+import { createGuid } from '../Utils';
 
-export default function AddExerciseModal(){
+interface props {
+    visible: boolean;
+    setVisibleFalse: () => void;
+    saveActionCallback: (element: exerciseElement) => void;
+    existingExercise?: exerciseElement
+}
+
+export default function AddExerciseModal(props: props){
+    let existingName: string = props.existingExercise != undefined ? props.existingExercise.exerciseName : "";
+    const [exerciseName, setExerciseName] = useState(existingName);
+
+    let existingReps: string = props.existingExercise != undefined ? props.existingExercise.exerciseReps.toString() : "";
+    const [exerciseReps, setExerciseReps] = useState(existingReps);
+    
+
+    function onSave(){
+        let newExerciseElement: exerciseElement = {
+            exerciseName: exerciseName,
+            exerciseReps: Number(exerciseReps),
+            key: createGuid(),
+        };
+        props.saveActionCallback(newExerciseElement);
+    }
+
+    function onCancel(){
+        props.setVisibleFalse();
+    }
+    
     return (
-        <Text>AddExerciseModal</Text>
+        <Modal
+        animationType="slide"
+        transparent={true}
+        visible={props.visible}
+        >
+            
+            <View style={{justifyContent: 'space-around'}}>
+                <Text>AddExerciseModal</Text>
+                <Text>Are you sure you want to delete: {props.workoutTitle}</Text>
+                <View style={{flexDirection:"row"}}>
+                    <TouchableOpacity onPress={onSave}><Text>Save</Text></TouchableOpacity>
+                    <TouchableOpacity onPress={onCancel}><Text>Cancel</Text></TouchableOpacity>
+                </View>
+            </View>
+
+        </Modal>
     );
 }
